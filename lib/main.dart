0f0834c0628d12de0885cc8c819f36be003b2a9e
peterkinuthia <peterchege442@gmail.com>
2019-09-24 20:29:12 +0300

    clean code with void, override and string definition

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
