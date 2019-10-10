2746d33dcef6660d9aabc4676f60b02acf4e5d1e
peterkinuthia <peterchege442@gmail.com>
2019-10-10 16:58:13 +0300

    adding a container

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,12 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              RaisedButton(
-                onPressed: () {},
-                child: Text('Add Products'),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Products'),
+                ),
               ),
               Card(
                 child: Column(
