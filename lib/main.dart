374250782bc868e0dfc7baf4c293e5abd8d947ec
peterkinuthia <peterchege442@gmail.com>
2019-10-15 21:09:23 +0300

    add method to the list of product

--- lib/main.dart
+++ lib/main.dart
@@ -10,30 +10,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: Text('EasList'),
+            title: Text('EasyList'),
           ),
           body: Column(
             children: [
               Container(
                 margin: EdgeInsets.all(10.0),
                 child: RaisedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      _products.add('Advance Food Tester');
+                    });
+                  },
                   child: Text('Add Products'),
                 ),
               ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text('food paradise')
-                  ],
-                ),
-              ),
+              Column(
+                children: _products
+                    .map(
+                      (element) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/food.jpg'),
+                            Text(element)
+                          ],
+                        ),
+                      ),
+                    )
+                    .toList(),
+              )
             ],
           )),
     );
